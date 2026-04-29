54ef7f47235f3214fb68e6e47644fb21603b79ca
Osemwengie Arit Bright <uyearit23@gmail.com>
2026-04-29 07:00:42 +0200

    Initial AritBiz setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>AritBiz is live 🚀</h1>"
+    return "<h1>AritBiz is live 💃</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
